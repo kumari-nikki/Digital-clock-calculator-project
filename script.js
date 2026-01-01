27ab8197b889ea30a270adb2d9fe3cd3ab802586
Kumari Nikki <nikkibellla66@gmail.com>
2026-01-01 17:31:40 +0530

    Implement real-time digital clock with date, day, AM/PM and responsive UI

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+let Months = [
+  "January", "February", "March", "April", "May", "June", 
+  "July", "August", "September", "October", "November", "December"
+];
+
+let Days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+let hours=document.getElementById("hrs");
+let minutes=document.getElementById("mins");
+let second=document.getElementById("seconds");
+let date=document.getElementById("date");
+let month=document.getElementById("month");
+let day=document.getElementById("day");
+let year=document.getElementById("year");
+let ampm=document.getElementById("am-pm");
+setInterval(()=>{
+let time = new Date();
+hours.innerHTML=(time.getHours()<10?"0":"")+time.getHours();
+minutes.innerHTML=(time.getMinutes()<10?"0":"")+time.getMinutes();
+second.innerHTML=(time.getSeconds()<10?"0":"")+time.getSeconds();
+date.innerHTML=time.getDate();
+month.innerText=Months[time.getMonth()];
+year.innerHTML=time.getFullYear();
+day.innerText=Days[time.getDay()];
+ampm.innerHTML=time.getHours()>=12?"PM":"AM";
+},1000)
+
+
+
 
